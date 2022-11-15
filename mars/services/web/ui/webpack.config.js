bc250abdcc5bab650334e6c2c7bad39a4a53e9f8
ChengjieLi <chengjieli23@outlook.com>
2022-11-15 16:06:00 +0800

    change to production

--- mars/services/web/ui/webpack.config.js
+++ mars/services/web/ui/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/App.js',
-  mode: process.env.NODE_ENV || "development",
+  mode: "production",
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
